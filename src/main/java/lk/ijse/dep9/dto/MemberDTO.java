e5826fa9d79515fba4b1f13d8123628f00ccf2a5
hiranwj <hiranwj.connect@gmail.com>
2023-02-02 02:14:23 +0530

    Finalize MemberDTO.java

--- src/main/java/lk/ijse/dep9/dto/MemberDTO.java
+++ src/main/java/lk/ijse/dep9/dto/MemberDTO.java
@@ -1,6 +1,67 @@
 package lk.ijse.dep9.dto;
 
-public class MemberDTO {
+import jakarta.json.bind.annotation.JsonbPropertyOrder;
 
+import java.io.Serializable;
 
+@JsonbPropertyOrder({"id", "name", "address", "contact"})
+public class MemberDTO implements Serializable {
+
+    /* Encapsulated instance variables */
+    private String id;
+    private String name;
+    private String address;
+    private String contact;
+
+    public MemberDTO() {
+    }
+
+    public MemberDTO(String id, String name, String address, String contact) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", contact='" + contact + '\'' +
+                '}';
+    }
 }
