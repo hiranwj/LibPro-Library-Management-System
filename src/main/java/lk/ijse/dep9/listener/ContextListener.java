8ab901072d41d9f4ca6e9175179bf57f62ceaa9f
hiranwj <hiranwj.connect@gmail.com>
2023-02-05 12:37:56 +0530

    remove ContextListener (@WebListener)

--- src/main/java/lk/ijse/dep9/listener/ContextListener.java
+++ src/main/java/lk/ijse/dep9/listener/ContextListener.java
@@ -3,10 +3,10 @@ package lk.ijse.dep9.listener;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
-import lk.ijse.dep9.db.ConnectionPool;
 import org.apache.commons.dbcp2.BasicDataSource;
 
-@WebListener
+/* Further no need web listener, because have inbuilt production grade connection pool in every application server(Tomcat, Glassfish ...) */
+//@WebListener
 public class ContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
